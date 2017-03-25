fb1560059a8d30e2d98f264d78673d5d51e6c88c
Laura L Watkins <lauralwatkins@gmail.com>
2017-03-25 12:07:12 -0400

    Modify init file.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-import os
-import string
-
-for f in os.listdir( os.path.dirname( __file__ ) ):
-    name, ext = string.split( f, "." )
-    if ( name != "__init__" and ext == "py" ):
-        tmp = __import__( name, locals(), globals(), [name], -1 )
-        vars()[name] = vars(tmp)[name]
-
-del f, name, ext, os, string
+from .bin1d import bin1d
+from .bin_stats import bin_stats
+from .estimator import estimator
+from .ll import ll
+from .maxlh import maxlh
+from .mcerror import mcerror
